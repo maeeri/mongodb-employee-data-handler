b9968c882c2a4ae7c0067bc3328ec8924f564264
Mari-Anne <107042762+maeeri@users.noreply.github.com>
2025-02-09 13:05:06 +0200

    get all results from MongoDb

--- main.py
+++ main.py
@@ -4,8 +4,7 @@ from src.MDbContext import MongoDbDataHandler
 collection = "employees"
 
 if __name__ == "__main__":
-    pDb = PGDataHandler()
-    employees = pDb.get_all_employees()
+    # pDb = PGDataHandler()
+    # employees = pDb.get_all_employees()
     mDb = MongoDbDataHandler()
-    mDb.clear_collection(collection)
-    mDb.insert_collection(collection, employees)
+    print(mDb.get_all(collection))
--- src/MDbContext.py
+++ src/MDbContext.py
@@ -4,6 +4,7 @@ from bson.objectid import ObjectId
 
 config = dotenv_values()
 
+
 class MongoDbDataHandler:
     def get_database(self):
         CONNECTION_STRING = config["MONGO_CONNECTION"]
@@ -11,10 +12,8 @@ class MongoDbDataHandler:
         client = MongoClient(CONNECTION_STRING)
         return client[DBNAME]
 
-
     def clear_collection(self, collection: str):
         self.get_database().drop_collection(collection)
-
 
     def insert_collection(self, collection: str, items):
         db = self.get_database()
@@ -24,3 +23,19 @@ class MongoDbDataHandler:
         else:
             db.create_collection(collection).insert_many(items)
 
+    def get_all(self, collection: str):
+        output = []
+        for employee in self.get_database().get_collection(collection).find():
+            output.append(self.employee_id_helper(employee))
+        return output
+
+    def employee_id_helper(self, employee) -> dict:
+        return {
+            "id": str(employee["_id"]),
+            "gender": employee["gender"],
+            "jobTitle": employee["jobTitle"],
+            "educationLevel": employee["educationLevel"],
+            "salary": employee["salary"],
+            "age": employee["age"],
+            "yearsOfExperience": employee["yearsOfExperience"],
+        }
